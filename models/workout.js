6760f2b885f9ba2fd0e1c613d3210bf8d4b6f3eb
Kelsey <BergstromKelsey@gmail.com>
2021-02-23 21:04:07 -0600

    PieChartBug

---
+++ models/workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+//creating a new database with collections, documents and feild names 
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Please enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Please enter the name of your exercise"
+        },
+        duration: {
+          type: Number,
+          required: "Please enter how many minutes you worked out today"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+);
+
+//total time of workouts
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
