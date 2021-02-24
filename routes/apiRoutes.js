3628b7111a87822fd6f4512288c634c599bb662b
Kelsey <BergstromKelsey@gmail.com>
2021-02-24 09:48:12 -0600

    MoreApiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 var db =require("../models");
 module.exports = function (app){
 
-    app.get("/api/workouts",(req,res)=> {
+app.get("/api/workouts",(req,res)=> {
 
 db.workout.find({})
 .then(workout => {
@@ -9,8 +9,31 @@ db.workout.find({})
 })
 .catch(err =>{
     res.json(err);
+    });
 });
 
-
+app.post("/api/workouts", async(req,res)=> {
+try{ 
+    const response= await db.workout.create({type:"workouts"})
+    res.json(response);
+}
+catch(err){
+     console.log("error")
+    }
+           
     });
+
+
+app.get("/api/workouts/range", (req,res)=>{
+
+    db.workout.find({})
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err =>{
+        res.json(err);
+        });
+    });
+
+
 }
