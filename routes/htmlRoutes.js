f98288ad80be59e1fb56fbd9cce164c01e86c133
Kelsey <BergstromKelsey@gmail.com>
2021-02-24 08:53:01 -0600

    ReconfigApiRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,16 @@
-var path = require("path");
-//connecting all the routes to each of our HTML pages!
-module.exports = function(app) {
+//requiring the npm path and connecting all of our html routes
 
-  app.get("/exercise", function(req, res) {
+
+var path= require("path");
+module.exports = function (app) {
+
+  app.get("/exercise", function(req,res){
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-
-  app.get("/", function(req, res) {
+  })
+  app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
-  app.get("/stats", function(req, res) {
+  })
+  app.get("/stats", function(req,res){
     res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-};
+  })
+}
